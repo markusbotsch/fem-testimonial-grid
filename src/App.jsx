81244423779fd4e0eada5add09edde114ee59093
Markus Botsch <me@markusbotsch.com>
2022-08-22 08:24:34 +0200

    Challenge completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { data } from './data.jsx';
+
+import Testimonial from './components/Testimonial';
 
 function App() {
 
+  const testimonials = data.map(testimonial => 
+    <Testimonial 
+      name={testimonial.name} 
+      image={testimonial.image}
+      headline={testimonial.headline}
+      copy={testimonial.copy}
+    />
+  )
+
   return (
-    <div className="App">
-      
+    <div className="testimonial-grid">
+       {testimonials}
     </div>
   )
 }
---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,16 @@
+export default function Testimonial({ name, image, headline, copy }) {
+
+    return(
+        <div className="testimonial">
+            <div className="personal-info">
+                <img src={`../images/${image}`} />
+                <div className="personal-info-data">
+                    <h3 className="personal-info-name">{name}</h3>
+                    <p className="opacity-50">Verfified Graduate</p>
+                </div>
+            </div>
+            <h2>{headline}</h2>
+            <p className="opacity-70">" {copy} "</p>
+        </div>
+    )
+}
