4870a87a38b3a9c15a2e2d7d6d4b645e2d4a0aed
Markus Botsch <me@markusbotsch.com>
2022-08-22 10:17:03 +0200

    Added alt-text to image-tag

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,7 +3,7 @@ export default function Testimonial({ name, image, headline, copy }) {
     return(
         <div className="testimonial">
             <div className="personal-info">
-                <img src={`../images/${image}`} />
+                <img src={`../images/${image}`} alt={name}/>
                 <div className="personal-info-data">
                     <h3 className="personal-info-name">{name}</h3>
                     <p className="opacity-50">Verfified Graduate</p>
